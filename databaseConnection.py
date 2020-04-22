6704cd1aeb0b23dda1b29a41fb174d7f74920c92
Vishwa Priya J <vishprij@thoughtworks.com>
2020-04-22 21:43:43 +0530

    Vishwa Add script to create price table

--- databaseConnection.py
+++ databaseConnection.py
@@ -13,12 +13,18 @@ def create_product_table(conn):
     conn.commit()
     cur.close()
 
+def create_price_table(conn):
+    cur = conn.cursor()
+    sql_str = "create table if not exists price (id VARCHAR(255) REFERENCES product(product_id),price DECIMAL, updated_time TIMESTAMP, " \
+              "active BOOL, price_product_unique_id VARCHAR(255))"
+    sql_resp = cur.execute(sql_str)
+    conn.commit()
+    cur.close()
 
 def insert_into_product_table(conn):
     cur = conn.cursor()
     with open('product-names', 'r') as f:
         product_names = f.read().split('\n')
-    print(product_names)
     elasticities = ['highly-elastic', 'medium-elastic', 'medium-inelastic', 'highly-inelastic']
     competitive_intensities = ['highly-competitive', 'competitive', 'captive', 'highly-captive']
     product_classifications = ['category-A', 'category-B', 'category-C', 'category-D']
@@ -37,57 +43,62 @@ def insert_into_product_table(conn):
                       'cost': uniform(100, 5000),
                       'start_date': (datetime.datetime.now() - datetime.timedelta(days=randint(1, 1000))).strftime("%Y-%m-%d")}
         records.append(new_record)
-    insert_sql_statement = create_insert_records(records, 'product')
+    insert_the_records(conn, cur, records, 'product')
+
+def insert_into_price_table(conn):
+    cur = conn.cursor()
+    with open('product-names', 'r') as f:
+        product_names = f.read().split('\n')
+    booleanValue = ['true', 'false']
+    records = []
+    for index, product_name in enumerate(product_names):
+        product_id = "p" + str(index)
+        price = uniform(100, 5000)
+        price_product_unique_id = product_id+str(price)
+        new_record = {'product_id': product_id,
+                      'price': price,
+                      'updated_time': (datetime.datetime.now() - datetime.timedelta(days=randint(1, 1000))).strftime("%Y-%m-%d"),
+                      'active': booleanValue[
+                        randint(0, len(booleanValue) - 1)],
+                      'price_product_unique_id': price_product_unique_id
+                      }
+        records.append(new_record)
+    insert_the_records(conn, cur, records, 'price')
+
+
+def insert_the_records(conn, cur, records, table_name):
+    insert_sql_statement = create_insert_records(records, table_name)
     cur.execute(insert_sql_statement)
     conn.commit()
     cur.close()
 
 
 def create_insert_records( json_array, table_name ):
-
     columns = json_array[0].keys()
     print ("\ncolumns:", columns)
-
     sql_string = "INSERT INTO {}".format(table_name)
     sql_string = sql_string + " (" + ', '.join(columns) + ")\nVALUES "
-
     record_list = []
-    for i, record in enumerate( json_array ):
 
+    for i, record in enumerate( json_array ):
         values = record.values()
         record = list(values)
         print(record)
 
-        # fix the values in the list if needed
         for i, val in enumerate(record):
-
             if type(val) == str:
                 if "'" in val:
-                    # posix escape string syntax for single quotes
                     record[i] = "E'" + record[i].replace("'", "''") + "'"
 
-        # cast record as string and remove the list brackets []
         record = str(record).replace("[", '')
         record = record.replace("]", '')
-
-        # remove double quotes as well
         record = record.replace('"', '')
-
-        # ..now append the records to the list
         record_list += [record]
 
-    # enumerate() over the records and append to SQL string
     for i, record in enumerate(record_list):
-
-        # use map() to cast all items in record list as a string
-        #record = list(map(str, record))
-
-        # append the record list of string values to the SQL string
         sql_string = sql_string + "(" + record + "),\n"
 
-    # replace the last comma with a semicolon
     sql_string = sql_string[:-2] + ";"
-
     return sql_string
 
 
@@ -101,8 +112,10 @@ try:
         connect_timeout=3
     )
 
-    create_product_table(conn)
-    insert_into_product_table(conn)
+    # create_product_table(conn)
+    # insert_into_product_table(conn)
+    create_price_table(conn)
+    insert_into_price_table(conn)
 
 
 except Error as err:
