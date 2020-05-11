7487f62a1ad589f005505bb7a8f1eca7456590d5
raghavmac <rghv.rocking@gmail.com>
2020-05-12 00:01:12 +0530

    add elasticsearch and kibana docker image

--- yarn-cluster/build-image.sh
+++ yarn-cluster/build-image.sh
@@ -2,7 +2,15 @@
 
 echo ""
 
-echo -e "\nbuild docker hadoop & spark image\n"
+echo -e "💥 build docker hadoop & spark image"
 docker build -t spark-hadoop:latest -f yarn-cluster/Dockerfile .
+
+echo -e "🔎 build elasticsearch"
+docker rm elasticsearch -f
+docker run --init --network hadoop -d --name elasticsearch -p 9200:9200 blacktop/elasticsearch
+
+echo -e "🎃 build kibana"
+docker rm kibana -f
+docker run --init --network hadoop -d --name kibana --link elasticsearch -p 5601:5601 blacktop/kibana
 
 echo ""
