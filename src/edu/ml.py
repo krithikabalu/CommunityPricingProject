18c9bfe14efd5aefc61b69e72ef9b1d971646301
Krithika Balu <krithikb@thoughtworks.com>
2020-07-06 23:35:01 +0530

    Krithika - Predict price using Spark MLLib

--- src/edu/ml.py
+++ src/edu/ml.py
@@ -6,20 +6,26 @@ spark = SparkSession.builder.appName("Demo").getOrCreate()
 
 
 def execute():
-    # data = spark.read.csv('/Users/krithikab/Desktop/PRACT/SparkML/kc_house_data.csv', header=True, inferSchema=True)
-    data = spark.read.csv('hdfs://hadoop-master:9000/user/root/kc_house_data.csv', header=True, inferSchema=True)
+    input_data = spark.read.csv('hdfs://hadoop-master:9000/kc_house_data.csv', header=True, inferSchema=True)
+    # input_data = spark.read.csv('/Users/krithikab/Desktop/PRACT/SparkML/kc_house_data.csv', header=True, inferSchema=True)
+
+    data = input_data\
+        .filter(input_data.price > 0)\
+        .withColumn("age", 2020-input_data.yr_built)\
+        .drop_duplicates()
+
     assembler = VectorAssembler() \
-        .setInputCols(["bedrooms", "bathrooms"]) \
+        .setInputCols(
+        ["bedrooms", "bathrooms", "sqft_living", "floors", "condition", "sqft_lot",
+         "waterfront", "view", "grade", "sqft_above", "sqft_basement", "age",
+         "zipcode", "lat", "long", "sqft_living15", "sqft_lot15"]) \
         .setOutputCol("features") \
         .transform(data)
-    assembler.show()
 
     normalizer = Normalizer() \
         .setInputCol("features") \
         .setOutputCol("normFeatures") \
-        .setP(2.0) \
         .transform(assembler)
-    normalizer.show()
 
     linear_regression = LinearRegression() \
         .setLabelCol("price") \
@@ -30,7 +36,10 @@ def execute():
 
     result_array = normalizer.randomSplit([0.7, 0.3])
     lr_model = linear_regression.fit(result_array[0])
+
     predicted_data = lr_model.transform(result_array[1]).select("features", "normFeatures", "price", "prediction")
-    predicted_data.show()
+    # predicted_data.select("price", "prediction").write.csv("result.csv")
+    predicted_data.select("price", "prediction").write.csv("hdfs://hadoop-master:9000/prediction.csv")
+
 
 execute()
