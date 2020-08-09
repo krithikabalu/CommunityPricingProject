d306f082ffb7a56b2f3bf467f7180f3ad2fe60f9
Krithika Balu <krithikb@thoughtworks.com>
2020-08-09 20:47:10 +0530

    Add word count example for map reduce

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 docker exec -it hadoop-master hadoop fs -rm -r /Output
 docker cp src/edu/pricing.py hadoop-master:/root/
 docker cp src/edu/error_processing.py hadoop-master:/root/
+docker cp src/WordCount-1.0-SNAPSHOT.jar hadoop-master:/root/
 docker exec -it hadoop-master spark-submit \
  --packages org.apache.spark:spark-avro_2.11:2.4.5 \
  --deploy-mode cluster \
@@ -11,3 +12,4 @@ docker exec -it hadoop-master spark-submit \
  --deploy-mode cluster \
   --num-executors 3 \
    /root/pricing.py hdfs://hadoop-master:9000/user/root/processed/product/ hdfs://hadoop-master:9000/Output/
+docker exec -it hadoop jar WordCount-1.0-SNAPSHOT.jar WordCount.Driver /sample1.txt /output
